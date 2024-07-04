e64a7fcd2b31328d4ed5d81d66f9ae2b65f78f75
Felicio <feliciorar@gmail.com>
2024-07-04 16:13:23 -0300

    fix git

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -5,14 +5,9 @@ import { TransactionsTable } from "../TransactionsTable";
 
 export function DashBoard() {
     return (
-        <main className="flex flex-col max-w-[1120px] w-full m-0 px-4 py-10">
+        <main className="flex flex-col max-w-[1120px] w-full m-0 px-4 py-10 pb-2 min-h-screen">
             <Summary />
             <TransactionsTable />
-            <div className="self-center">
-                <span>
-                    Github
-                </span>
-            </div>
         </main>
     )
 }
